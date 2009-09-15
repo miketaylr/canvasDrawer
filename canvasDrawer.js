17a43d731fe2827663e486368c46e77ef307f685
Mike Taylor <runningtowardsnothing@gmail.com>
2009-09-14 21:52:09 -0400

    adding copyright, license info

---
+++ canvasDrawer.js
@@ -0,0 +1,13 @@
+/***
+* @author Mike Taylor
+* @copyright Copyright (c) 2009 Mike Taylor
+* @license http://www.opensource.org/licenses/mit-license.php
+* @version 0.0.1
+*/
+
+var canvasDrawer = function(canvas_id) {
+		var cd_canvas = document.getElementById(canvas_id);
+		var cd_context = cd_canvas.getContext("2d");
+		
+		
+};
