ae0512ba95d56dff093b4263c4c5064fa82bbfc5
Mike Taylor <runningtowardsnothing@gmail.com>
2009-09-18 00:56:32 -0400

    making move functions more DRY

--- canvasDrawer.js
+++ canvasDrawer.js
@@ -8,85 +8,54 @@
 */
 
 // namespace
-var _MT = _MT || {};
+var _MT = {};
 
 (function() {
 	
-	_MT.CanvasDrawer = function(canvas_id) {
+	_MT.CanvasDrawer = function(options) {
 		// grab canvas element
-		var canvas = document.getElementById(canvas_id);
+		var canvas = document.getElementById(options._id);
 		
 		if (canvas && canvas.getContext) {
 			//and set its 2d context, after testing for support
 			var context = canvas.getContext("2d");
 
 			// set up private context props for drawing.
-			context.lineWidth = 3;
-			context.strokeStyle = "#369"
-			context.lineCap = "round";
-			context.lineJoin = "round";
+			context.lineWidth = options.lineWidth;
+			context.strokeStyle = options.strokeStyle;
 			
 			return {
 				//initial position
 				pX: 40,
 				pY: 40,
-					
-				up: function() {
+				
+				//generic move function
+				move: function(changeX, changeY) {
 					context.beginPath();
 					context.moveTo(this.pX,this.pY);
 					
-					//update target position
-					this.pY -= 100;
+					this.pX += changeX;
+					this.pY += changeY;
 					
 					context.lineTo(this.pX,this.pY);
 					context.stroke();
-					
 					return this;
+				},
+				
+				up: function() {
+					this.move(0,-100);
 				},
 
 				right: function() {
-					//draw a line to the right
-					
-					context.beginPath();
-					context.moveTo(this.pX,this.pY);
-					
-					//update target position
-					this.pX += 100;
-					
-					context.lineTo(this.pX,this.pY);
-					context.stroke();
-					
-					return this;
+					this.move(100,0);
 				},
 
 				down: function() {
-					//draw a line down
-					
-					context.beginPath();
-					context.moveTo(this.pX,this.pY);
-					
-					//update target position
-					this.pY += 100;
-					
-					context.lineTo(this.pX,this.pY);
-					context.stroke();
-
-					return this;
+					this.move(0,100);
 				},
 
 				left: function() {
-					//draw a line to the left
-					
-					context.beginPath();
-					context.moveTo(this.pX,this.pY);
-					
-					//update target position
-					this.pX -= 100;
-					
-					context.lineTo(this.pX,this.pY);
-					context.stroke();
-					
-					return this;
+					this.move(-100,0);
 				},
 
 				clear: function() {
